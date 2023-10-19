35239df503a2d8d5b9dd0b3441a2d38d23c6b59b
Chris Puzzuoli <cpuzzuol@umich.edu>
2023-10-19 13:58:00 -0400

    build

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,18 +12,30 @@
           />
         </q-toolbar-title>
         <q-tabs v-model="tabToolbar" class="q-mr-xl">
-          <q-tab name="whyuc" label="Services" @click="scrollToAnchor('why-uc-section')" />
-          <q-tab name="capabilities" label="Capabilities" @click="scrollToAnchor('capabilities-section')" />
-          <q-tab name="clients" label="Clients" @click="scrollToAnchor('clients-section')" />
+          <q-tab
+            name="whyuc"
+            label="Services"
+            @click="scrollToAnchor('why-uc-section')"
+          />
+          <q-tab
+            name="capabilities"
+            label="Capabilities"
+            @click="scrollToAnchor('capabilities-section')"
+          />
+          <q-tab
+            name="clients"
+            label="Clients"
+            @click="scrollToAnchor('clients-section')"
+          />
         </q-tabs>
-        <q-btn dense flat label="Our Team"/>
-        <q-btn dense label="Get Started" id="get-started-btn" class="q-ml-sm"/>
+        <q-btn dense flat label="Our Team" />
+        <q-btn dense label="Get Started" id="get-started-btn" class="q-ml-sm" />
         <!--        <q-btn flat @click="drawerRight = !drawerRight" round dense icon="menu" />-->
       </q-toolbar>
     </q-header>
 
     <q-page-container>
-      <router-view/>
+      <router-view />
     </q-page-container>
 
     <q-footer reveal bordered class="bg-grey-8 text-white">
@@ -38,18 +50,16 @@
       <div class="row justify-center">
         <div class="col-xs-12 col-md-3">First column</div>
         <div class="col-xs-12 col-md-6 text-center">
-          <q-form
-            @submit="onSubmit"
-            @reset="onReset"
-            class="q-gutter-md"
-          >
+          <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
             <q-input
               filled
               v-model="name"
               label="Your name *"
               hint="Name and surname"
               lazy-rules
-              :rules="[ val => val && val.length > 0 || 'Please type something']"
+              :rules="[
+                (val) => (val && val.length > 0) || 'Please type something',
+              ]"
             />
 
             <q-input
@@ -59,16 +69,22 @@
               label="Your age *"
               lazy-rules
               :rules="[
-                val => val !== null && val !== '' || 'Please type your age',
-                val => val > 0 && val < 100 || 'Please type a real age'
+                (val) => (val !== null && val !== '') || 'Please type your age',
+                (val) => (val > 0 && val < 100) || 'Please type a real age',
               ]"
             />
 
-            <q-toggle v-model="accept" label="I accept the license and terms"/>
+            <q-toggle v-model="accept" label="I accept the license and terms" />
 
             <div>
-              <q-btn label="Submit" type="submit" color="primary"/>
-              <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"/>
+              <q-btn label="Submit" type="submit" color="primary" />
+              <q-btn
+                label="Reset"
+                type="reset"
+                color="primary"
+                flat
+                class="q-ml-sm"
+              />
             </div>
           </q-form>
         </div>
@@ -79,16 +95,16 @@
 </template>
 
 <script>
-import {useQuasar} from 'quasar'
-import {ref, onMounted} from "vue"
+import { useQuasar } from "quasar";
+import { ref, onMounted } from "vue";
 
 export default {
   setup() {
-    const $q = useQuasar()
+    const $q = useQuasar();
 
-    const name = ref(null)
-    const age = ref(null)
-    const accept = ref(false)
+    const name = ref(null);
+    const age = ref(null);
+    const accept = ref(false);
 
     return {
       name,
@@ -96,48 +112,48 @@ export default {
       accept,
       drawerLeft: ref(false),
       drawerRight: ref(false),
-      tabToolbar: ref('whyuc'),
+      tabToolbar: ref("whyuc"),
 
       onSubmit() {
         if (accept.value !== true) {
           $q.notify({
-            color: 'red-5',
-            textColor: 'white',
-            icon: 'warning',
-            message: 'You need to accept the license and terms first'
-          })
+            color: "red-5",
+            textColor: "white",
+            icon: "warning",
+            message: "You need to accept the license and terms first",
+          });
         } else {
           $q.notify({
-            color: 'green-4',
-            textColor: 'white',
-            icon: 'cloud_done',
-            message: 'Submitted'
-          })
+            color: "green-4",
+            textColor: "white",
+            icon: "cloud_done",
+            message: "Submitted",
+          });
         }
       },
 
       onReset() {
-        name.value = null
-        age.value = null
-        accept.value = false
+        name.value = null;
+        age.value = null;
+        accept.value = false;
       },
 
       scrollToAnchor(anchor) {
         // Get a reference to the anchor element
-        const anchorElement = document.getElementById(anchor)
+        const anchorElement = document.getElementById(anchor);
 
         // Check if the anchor element exists
         if (anchorElement) {
-          console.log(anchorElement)
+          console.log(anchorElement);
           // Scroll to the anchor element using JavaScript
           anchorElement.scrollIntoView({
-            behavior: 'smooth', // Add smooth scrolling
-            block: 'start',     // Scroll to the top of the element
+            behavior: "smooth", // Add smooth scrolling
+            block: "start", // Scroll to the top of the element
           });
         }
-      }
-    }
-  }
+      },
+    };
+  },
 };
 </script>
 
@@ -148,12 +164,12 @@ export default {
   max-height: 5.5rem;
   margin-top: 1.5rem;
   text-align: center;
-  @include respond-to('smallAndUp') {
+  @include respond-to("smallAndUp") {
     background-color: $ucBkgDark;
     border-top: 5px solid $ucGoldTrim;
     border-bottom: 5px solid $ucGoldTrim;
   }
-  @include respond-to('mediumAndUp') {
+  @include respond-to("mediumAndUp") {
     max-height: 125px;
     margin-top: 20px;
   }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,108 +4,120 @@
       <h1 class="text-h2 q-mb-xl">UCrisko, Academic and Web Solutions</h1>
     </div>
 
-    <div id="why-uc-section" ref="why-uc-section" class="row uc-section text-white q-pa-lg">
+    <div
+      id="why-uc-section"
+      ref="why-uc-section"
+      class="row uc-section text-white q-pa-lg"
+    >
       <div class="col-xs-12 col-md-6">
         <h2 id="h2-main">Academic and Business Web Solutions</h2>
         <h3 id="h3-main">Your future starts right here.</h3>
         <p>
-          Whether your goal is to educate the minds of tomorrow or take your business technology to the next level, UCrisko's expertise can help get you there.
-          Contact us today to get started on imagining the next step of your journey.
+          Whether your goal is to educate the minds of tomorrow or take your
+          business technology to the next level, UCrisko's expertise can help
+          get you there. Contact us today to get started on imagining the next
+          step of your journey.
         </p>
         <q-btn
           color="white"
           class="text-black"
           href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."
           tabindex="2"
-        >Let's get started!
+          >Let's get started!
         </q-btn>
       </div>
-<!--      <div class="flex justify-center q-mx-xl">-->
-<!--        <div class="row q-gutter-lg">-->
-<!--          <q-card-->
-<!--            bordered-->
-<!--            class="bg-grey-1 why-uc-card flex justify-center items-center text-center"-->
-<!--          >-->
-<!--            <q-card-section>-->
-<!--              <div class="text-h6">Industry Expertise</div>-->
-<!--              <q-icon name="business" class="text-red-10" size="5em"/>-->
-<!--            </q-card-section>-->
-<!--            <q-card-section>-->
-<!--              From automotive to retail, our team builds applications tailored-->
-<!--              to the needs of our clients.-->
-<!--            </q-card-section>-->
-<!--            <q-card-section>-->
-<!--              <q-btn-->
-<!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
-<!--                tabindex="1"-->
-<!--              >Prove it-->
-<!--              </q-btn-->
-<!--              >-->
-<!--            </q-card-section>-->
-<!--          </q-card>-->
-<!--          <q-card-->
-<!--            dark-->
-<!--            bordered-->
-<!--            class="bg-blue-grey-10 why-uc-card flex justify-center items-center text-center"-->
-<!--          >-->
-<!--            <q-card-section>-->
-<!--              <div class="text-h6">Focused on the Future</div>-->
-<!--              <q-icon name="school" class="text-red-10" size="5em"/>-->
-<!--            </q-card-section>-->
-<!--            <q-card-section>-->
-<!--              We partner with Universities and academic institutions that shape-->
-<!--              the minds of tomorrow.-->
-<!--            </q-card-section>-->
-<!--            <q-card-section>-->
-<!--              <q-btn-->
-<!--                color="white"-->
-<!--                class="text-black"-->
-<!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
-<!--                tabindex="2"-->
-<!--              >Show me-->
-<!--              </q-btn-->
-<!--              >-->
-<!--            </q-card-section>-->
-<!--          </q-card>-->
-<!--          <q-card-->
-<!--            bordered-->
-<!--            class="bg-grey-1 why-uc-card flex justify-center items-center text-center"-->
-<!--          >-->
-<!--            <q-card-section>-->
-<!--              <div class="text-h6">Quality Meets Service</div>-->
-<!--              <q-icon name="groups" class="text-red-10" size="5em"/>-->
-<!--            </q-card-section>-->
+      <!--      <div class="flex justify-center q-mx-xl">-->
+      <!--        <div class="row q-gutter-lg">-->
+      <!--          <q-card-->
+      <!--            bordered-->
+      <!--            class="bg-grey-1 why-uc-card flex justify-center items-center text-center"-->
+      <!--          >-->
+      <!--            <q-card-section>-->
+      <!--              <div class="text-h6">Industry Expertise</div>-->
+      <!--              <q-icon name="business" class="text-red-10" size="5em"/>-->
+      <!--            </q-card-section>-->
+      <!--            <q-card-section>-->
+      <!--              From automotive to retail, our team builds applications tailored-->
+      <!--              to the needs of our clients.-->
+      <!--            </q-card-section>-->
+      <!--            <q-card-section>-->
+      <!--              <q-btn-->
+      <!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
+      <!--                tabindex="1"-->
+      <!--              >Prove it-->
+      <!--              </q-btn-->
+      <!--              >-->
+      <!--            </q-card-section>-->
+      <!--          </q-card>-->
+      <!--          <q-card-->
+      <!--            dark-->
+      <!--            bordered-->
+      <!--            class="bg-blue-grey-10 why-uc-card flex justify-center items-center text-center"-->
+      <!--          >-->
+      <!--            <q-card-section>-->
+      <!--              <div class="text-h6">Focused on the Future</div>-->
+      <!--              <q-icon name="school" class="text-red-10" size="5em"/>-->
+      <!--            </q-card-section>-->
+      <!--            <q-card-section>-->
+      <!--              We partner with Universities and academic institutions that shape-->
+      <!--              the minds of tomorrow.-->
+      <!--            </q-card-section>-->
+      <!--            <q-card-section>-->
+      <!--              <q-btn-->
+      <!--                color="white"-->
+      <!--                class="text-black"-->
+      <!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
+      <!--                tabindex="2"-->
+      <!--              >Show me-->
+      <!--              </q-btn-->
+      <!--              >-->
+      <!--            </q-card-section>-->
+      <!--          </q-card>-->
+      <!--          <q-card-->
+      <!--            bordered-->
+      <!--            class="bg-grey-1 why-uc-card flex justify-center items-center text-center"-->
+      <!--          >-->
+      <!--            <q-card-section>-->
+      <!--              <div class="text-h6">Quality Meets Service</div>-->
+      <!--              <q-icon name="groups" class="text-red-10" size="5em"/>-->
+      <!--            </q-card-section>-->
 
-<!--            <q-card-section>-->
-<!--              We are a client-focused company. We want to treat you to a-->
-<!--              first-class experience.-->
-<!--            </q-card-section>-->
+      <!--            <q-card-section>-->
+      <!--              We are a client-focused company. We want to treat you to a-->
+      <!--              first-class experience.-->
+      <!--            </q-card-section>-->
 
-<!--            <q-card-section>-->
-<!--              <q-btn-->
-<!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
-<!--                tabindex="3"-->
-<!--              >Let's talk-->
-<!--              </q-btn-->
-<!--              >-->
-<!--            </q-card-section>-->
-<!--          </q-card>-->
-<!--        </div>-->
-<!--      </div>-->
+      <!--            <q-card-section>-->
+      <!--              <q-btn-->
+      <!--                href="mailto:cpuzzuol@ucrisko.com?subject=Schedule a meeting&body=I came across your website, ucrisko.com, and would like to meet to discuss a web project."-->
+      <!--                tabindex="3"-->
+      <!--              >Let's talk-->
+      <!--              </q-btn-->
+      <!--              >-->
+      <!--            </q-card-section>-->
+      <!--          </q-card>-->
+      <!--        </div>-->
+      <!--      </div>-->
     </div>
     <div id="capabilities-section" class="row uc-section q-pa-lg items-center">
       <div id="capabilities-section-text" class="col-xs-12 col-md-5 col-lg-4">
         <h3 id="h3-main">Our Expertise. Your Dream.</h3>
-        From company websites to custom content management systems, UCrisko has the expertise to make your dream application a reality.
-        Even if you don't need a full web application, the UCrisko team can help you craft your organization's logo or re-design an existing site.
+        From company websites to custom content management systems, UCrisko has
+        the expertise to make your dream application a reality. Even if you
+        don't need a full web application, the UCrisko team can help you craft
+        your organization's logo or re-design an existing site.
       </div>
     </div>
     <div id="clients-section" class="row uc-section q-pa-lg">
       <div id="clients-section-text" class="col-xs-12 col-md-4 col-lg-3">
         <h3 id="h3-main">Case Studies</h3>
-        Check out some of our satisfied clients and discover how UCrisko helped them boost their online presence.
+        Check out some of our satisfied clients and discover how UCrisko helped
+        them boost their online presence.
       </div>
-      <div id="client-rotator-container" class="col-xs-12 col-md-8 col-lg-9 q-pl-xl">
+      <div
+        id="client-rotator-container"
+        class="col-xs-12 col-md-8 col-lg-9 q-pl-xl"
+      >
         Clients go here.
       </div>
     </div>
@@ -116,13 +128,13 @@
       :scroll-offset="150"
       :offset="[18, 18]"
     >
-      <q-btn fab icon="keyboard_arrow_up" color="red-10"/>
+      <q-btn fab icon="keyboard_arrow_up" color="red-10" />
     </q-page-scroller>
   </q-page>
 </template>
 
 <script>
-import {defineComponent, ref} from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
@@ -132,7 +144,7 @@ export default defineComponent({
       // slide2: ref('style'),
       // lorem: 'Lorem ipsum dolor, sit ameignissimos praesentium libero ab nemo.'
     };
-  }
+  },
 });
 </script>
 
@@ -164,11 +176,10 @@ export default defineComponent({
 }
 
 .uc-section {
-
 }
 #why-uc-section {
   background-color: $ucBkgDark;
-  @include respond-to('mediumAndUp') {
+  @include respond-to("mediumAndUp") {
     background-image: url("src/assets/it-globe.jpg");
     background-repeat: no-repeat;
     background-position: top right;
@@ -177,7 +188,7 @@ export default defineComponent({
 }
 #capabilities-section {
   background-color: #cccccc;
-  @include respond-to('mediumAndUp') {
+  @include respond-to("mediumAndUp") {
     background-image: url("src/assets/cube_text.png");
     background-repeat: no-repeat;
     background-position: center right;
@@ -186,7 +197,7 @@ export default defineComponent({
 }
 
 #clients-section {
-  @include respond-to('mediumAndUp') {
+  @include respond-to("mediumAndUp") {
     height: 250px;
   }
 }
